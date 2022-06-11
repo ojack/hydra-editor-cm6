bf2ed8100627bbc14bf90ec0488769d2670bd634
ojack <ojackson@gmail.com>
2022-06-11 18:45:53 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,15 @@
+WIP [hydra](https://hydra.ojack.xyz) editor built using Codemirror6
+
+Code inspiration from: https://github.com/folz/hiasynth/tree/main/packages/editor
+
+To implement:
+- flash highlighting on evaluation
+- autocomplete
+- custom keymaps
+- internationalization
+- bundle as module as well as standalone editor with iframe API that can be used along with hydra-synth
+
+Already implemented:
+- bracket matching
+- multi-selection
+- key command for commenting code
--- main.js
+++ main.js
@@ -1,19 +1,45 @@
 // import './style.css'
 
-import {EditorView, keymap} from "@codemirror/view"
+import { drawSelection, EditorView, keymap} from "@codemirror/view"
 import {EditorState} from "@codemirror/state"
 import {defaultKeymap, history, historyKeymap} from "@codemirror/commands"
-import {syntaxHighlighting, defaultHighlightStyle} from "@codemirror/language"
+import {syntaxHighlighting, defaultHighlightStyle, bracketMatching} from "@codemirror/language"
 import {javascript} from "@codemirror/lang-javascript"
+import { commentKeymap } from '@codemirror/comment';
+import { oneDark } from '@codemirror/theme-one-dark';
+import { highlightSelectionMatches, searchKeymap } from '@codemirror/search';
+import { autocompletion } from '@codemirror/autocomplete';
+
+// import { closeBrackets, closeBracketsKeymap } from '@codemirror/closebrackets';
+// import { highlightSelectionMatches, searchKeymap } from '@codemirror/search';
+// import { bracketMatching } from '@codemirror/matchbrackets';
 
 let myView = new EditorView({
   state: EditorState.create({
     doc: "osc()",
     extensions: [
       history(),
+      drawSelection(),
       javascript(),
+      // autocompletion(),
+      highlightSelectionMatches(),
       syntaxHighlighting(defaultHighlightStyle),
-      keymap.of(defaultKeymap)
+      bracketMatching(),
+      keymap.of([...defaultKeymap,   ...searchKeymap, ...commentKeymap, ...historyKeymap]),
+      EditorView.theme({
+        '&': {
+          backgroundColor: 'transparent',
+          fontSize: '20px',
+        },
+        '& .cm-line': {
+          maxWidth: 'fit-content',
+          background: 'hsla(50,23%,5%,0.6)',
+        },
+        '&.cm-focused': {
+          outline: 'none',
+        },
+      }),
+      oneDark
     ]
   }),
   parent: document.querySelector('#app')
