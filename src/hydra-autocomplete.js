b05cfbd7eb2ea4fee625b20f0cbe921bfe7ac340
ojack <ojackson@gmail.com>
2022-06-12 14:24:45 +0200

    working autocomplete

--- src/hydra-autocomplete.js
+++ src/hydra-autocomplete.js
@@ -1,137 +1,160 @@
-import {syntaxTree} from "@codemirror/language"
+import { syntaxTree } from "@codemirror/language"
 import hydraLang from './hydra-lang.js'
 
 const completePropertyAfter = ["PropertyName", ".", "?."]
 const dontCompleteIn = ["TemplateString", "LineComment", "BlockComment",
-                        "VariableDefinition", "PropertyDefinition"]
+  "VariableDefinition", "PropertyDefinition"]
 
 const getObjectFromName = (name = '') => {
-    const last = name.split('.')
-    return last[last.length - 1].replace('(', '').replace(')', '')
+  const last = name.split('.')
+  return last[last.length - 1].replace('(', '').replace(')', '')
+}
+
+const getFunctionText = (f = () => { }) => {
+  const str = f.toString()
+  return str.split("{")[0]
 }
 
 
 
+const createAutocomplete = ({ hydraConstants, srcOptions, externalSourceOptions, combineNames, chainOptions, outputOptions, hydraGlobals }) => (context) => {
+  let word = context.matchBefore(/\w*/)
 
-const createAutocomplete = ( { srcOptions, externalSourceOptions, combineNames, chainOptions, outputOptions, hydraGlobals }) => (context) => {
-    let word = context.matchBefore(/\w*/)
+  let nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1)
 
-    let nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1)
+  console.log('word', word, nodeBefore, nodeBefore.name)
 
-    console.log('word', word, nodeBefore, nodeBefore.name)
+  // if (word.from == word.to && !context.explicit) {
+  //   return null
+  // }
+  // } else {
 
-    // if (word.from == word.to && !context.explicit) {
-    //   return null
-    // }
-    // } else {
-
-    // }
-
-    console.log('before', word, nodeBefore, nodeBefore.name,  nodeBefore.parent?.name)
-    console.log('children', nodeBefore.parent?.getChildren())
-    console.log('exp', nodeBefore.parent?.getChild('Expression'))
-    console.log('exp2', nodeBefore.parent?.parent?.getChild('Expression'))
-    console.log('exp3', nodeBefore.parent?.parent?.getChild('VariableName'))
-    if(nodeBefore.parent?.name === 'ArgList') {
-        let object = nodeBefore.parent?.parent?.getChild('Expression')
-        let variableName = context.state.sliceDoc(object.from, object.to)
-        let lastValue = getObjectFromName(variableName)
-        console.log('last value', lastValue, combineNames)
-        if(lastValue === 'out') {
-          return { from: word.from, options: outputOptions }
-        }
-        if(combineNames.includes(lastValue)) {
-          return { from: word.from, options: [...srcOptions, ...outputOptions, ...externalSourceOptions ] }
-        }
-        if(lastValue === 'src') {
-          return { from: word.from, options: [...externalSourceOptions, ...outputOptions] }
-        }
-        return null
-      //   console.log('completing arguments', variableName, lastValue)
-      //  // let object2 = nodeBefore.parent?.parent?.getChild('MemberExpression')
-      //  // let variableName2 = context.state.sliceDoc(object2.from, object2.to)
-      //  // console.log('completing arguments2', variableName2)
-      //   // let variableName = context.state.sliceDoc(object.from, object.to)
-      //   console.log('completing arguments', nodeBefore.name, object)
-      //   return {
-      //       from: word.from,
-      //       options: [{ label: 'o0'}]
-      //   }
+  // }
+  const parseArguments = (node) => {
+    let object = node.parent?.getChild('Expression')
+    let variableName = context.state.sliceDoc(object.from, object.to)
+    let lastValue = getObjectFromName(variableName)
+    console.log('last value', lastValue, combineNames)
+    if (lastValue === 'out') {
+      return outputOptions
     }
-     if (completePropertyAfter.includes(nodeBefore.name) &&
-         nodeBefore.parent?.name == "MemberExpression") {
-         let object = nodeBefore.parent.getChild("Expression")
-         console.log('expression name', object?.name)
-         if(object?.name === "VariableName") {
-             // s0, s1, a,
-             let variableName = context.state.sliceDoc(object.from, object.to)
-              console.log('completing properties on object', variableName)
-         } else if (object?.name === "CallExpression") {
-            let variableName = context.state.sliceDoc(object.from, object.to)
-            // if(srcNames.includes(variableName)) {
-            //     console.log('hydra source!')
-            // }
-            return {
-                from: word.from,
-                options: chainOptions
-            }
-            console.log('completing expression on object', variableName)
-         }
-     }
-    //   if (object?.name == "VariableName") {
-    //     let from = /\./.test(nodeBefore.name) ? nodeBefore.to : nodeBefore.from
-    //     let variableName = context.state.sliceDoc(object.from, object.to)
-    //     console.log('completing properties on object', variableName)
-    //     if (typeof window[variableName] == "object")
-    //       return completeProperties(from, window[variableName])
-    //   }
-    // } else if (nodeBefore.name == "VariableName") {
-    //     console.log('completing properties in global scope', nodeBefore.name)
-    //  // return completeProperties(nodeBefore.from, window)
-    //     return {
-    //         from: word.from, options: [...functionOptions, ...options]
-    //     }
-    // } else if (context.explicit && !dontCompleteIn.includes(nodeBefore.name)) {
-    //     console.log(`don't autocomplete`)
-    //     return {
-    //         from: word.from, options: [...functionOptions, ...options]
-    //     }
-    //  // return completeProperties(context.pos, window)
-    // } else {
-    //     console.log(`didnt match any case`)
-    //     return null
+    if (combineNames.includes(lastValue)) {
+      return [...srcOptions, ...outputOptions, ...externalSourceOptions]
+    }
+  if (lastValue === 'src') {
+    return [...externalSourceOptions, ...outputOptions]
+  }
+  return []
+}
 
+console.log('before', nodeBefore.name, nodeBefore.parent?.name, nodeBefore.parent?.parent?.name,)
+console.log('children', nodeBefore.parent?.getChildren())
+console.log('exp', nodeBefore.parent?.getChild('Expression'))
+console.log('exp2', nodeBefore.parent?.parent?.getChild('Expression'))
+console.log('exp3', nodeBefore.parent?.parent?.getChild('VariableName'))
+if (nodeBefore.name === 'ArrowFunction' || nodeBefore.parent?.name === 'ArrowFunction') {
+  console.log('arrow function!')
+  return { from: word.from, options: [...hydraConstants] }
+}
+
+// filling in parameters inside parenthesis
+if (nodeBefore.parent?.name === 'ArgList') {
+  const options = parseArguments(nodeBefore.parent)
+  return { from: word.from, options }
+}
+
+if (nodeBefore.name === 'ArgList') {
+  const options = parseArguments(nodeBefore)
+  return { from: word.from, options }
+}
+
+// following a '.'
+if (completePropertyAfter.includes(nodeBefore.name) &&
+  nodeBefore.parent?.name == "MemberExpression") {
+  let object = nodeBefore.parent.getChild("Expression")
+  console.log('expression name', object?.name)
+  // object such as s0, s1, or a
+  if (object?.name === "VariableName") {
+    // s0, s1, a,
+    let variableName = context.state.sliceDoc(object.from, object.to)
+    console.log('completing properties on object', variableName)
+    const obj = window[variableName]
+    if (nodeBefore.parent?.parent?.name === 'ArrowFunction') {
+      const options = Object.keys(obj).map((o) => ({ label: o }))
+      console.log('calculating options', options)
+      return { from: word.from, options }
+    } else {
+      const options = Object.getOwnPropertyNames(Object.getPrototypeOf(obj)).map((o) => ({ label: o, info: getFunctionText(obj[o]) }))
+      return { from: word.from, options }
+    }
+    console.log('completeing props for', typeof obj, obj.constructor.name)
+    // after a hydra function such as osc()
+  } else if (object?.name === "CallExpression") {
+    let variableName = context.state.sliceDoc(object.from, object.to)
+    // if(srcNames.includes(variableName)) {
+    //     console.log('hydra source!')
     // }
     return {
       from: word.from,
-    //   options: [
-    //     {label: "osc()", apply: "osc()", type: "source", /*detail: "sync, offset, color",*/ info: "sync, offset, color"},
-    //     {label: "noise()", type: "source"},
-    //     { label: "out()"},
-    //     {label: "modulate()", type: "coord"},
-    //     {label: "hello", type: "variable", info: "(World)"},
-    //     {label: "magic", type: "text", apply: "⠁⭒*.✩.*⭒⠁", detail: "macro"}
-    //   ]
-        options: [...srcOptions, ...hydraGlobals]
+      options: chainOptions
     }
+    console.log('completing expression on object', variableName)
+  }
+}
+//   if (object?.name == "VariableName") {
+//     let from = /\./.test(nodeBefore.name) ? nodeBefore.to : nodeBefore.from
+//     let variableName = context.state.sliceDoc(object.from, object.to)
+//     console.log('completing properties on object', variableName)
+//     if (typeof window[variableName] == "object")
+//       return completeProperties(from, window[variableName])
+//   }
+// } else if (nodeBefore.name == "VariableName") {
+//     console.log('completing properties in global scope', nodeBefore.name)
+//  // return completeProperties(nodeBefore.from, window)
+//     return {
+//         from: word.from, options: [...functionOptions, ...options]
+//     }
+// } else if (context.explicit && !dontCompleteIn.includes(nodeBefore.name)) {
+//     console.log(`don't autocomplete`)
+//     return {
+//         from: word.from, options: [...functionOptions, ...options]
+//     }
+//  // return completeProperties(context.pos, window)
+// } else {
+//     console.log(`didnt match any case`)
+//     return null
+
+// }
+return {
+  from: word.from,
+  //   options: [
+  //     {label: "osc()", apply: "osc()", type: "source", /*detail: "sync, offset, color",*/ info: "sync, offset, color"},
+  //     {label: "noise()", type: "source"},
+  //     { label: "out()"},
+  //     {label: "modulate()", type: "coord"},
+  //     {label: "hello", type: "variable", info: "(World)"},
+  //     {label: "magic", type: "text", apply: "⠁⭒*.✩.*⭒⠁", detail: "macro"}
+  //   ]
+  options: [...srcOptions, ...hydraGlobals]
+}
   }
 
-  export default (hydra) => {
-    const autocompleteOptions = hydraLang(hydra)
-    return createAutocomplete(autocompleteOptions)
-  }
+export default (hydra) => {
+  const autocompleteOptions = hydraLang(hydra)
+  return createAutocomplete(autocompleteOptions)
+}
 
-  function completeProperties(from, object) {
-    let options = []
-    for (let name in object) {
-      options.push({
-        label: name,
-        type: typeof object[name] == "function" ? "function" : "variable"
-      })
-    }
-    return {
-      from,
-      options,
-      validFor: /^[\w$]*$/
-    }
+function completeProperties(from, object) {
+  let options = []
+  for (let name in object) {
+    options.push({
+      label: name,
+      type: typeof object[name] == "function" ? "function" : "variable"
+    })
   }
+  return {
+    from,
+    options,
+    validFor: /^[\w$]*$/
+  }
+}
--- src/hydra-lang.js
+++ src/hydra-lang.js
@@ -1,12 +1,22 @@
 import hydraFunctions from 'hydra-synth/src/glsl/glsl-functions'
 
+const filterOut = ['prev', 'sum']
+
+const getFunctionText = (f = () => {}) => {
+    const str = f.toString()
+    return str.split("{")[0]
+  }
+
 export default (hydra) => {
-    const hydraGlobals = Object.entries(hydra.synth).map(([key, val]) => {
+    const synthParams = Object.entries(hydra.synth).map(([key, val]) => {
       console.log(key, val, typeof val)
       let label = key
       let type = typeof val
+      let info = ''
       if(type === "function") {
           label += '()'
+        //  info = getFunctionText(val)
+        //   info = val.toString()
       } else if (type === 'object') {
           type = val.constructor.name
       }
@@ -14,12 +24,14 @@ export default (hydra) => {
       return {
       label,
       type,
-      detail: type,
-      info: type
+      name: key,
+    //   detail: type,
+      info: info
+    //   info: type
       // info: JSON.stringify(val)
     }})
   
-    const hydraFuncs = hydraFunctions()
+    const hydraFuncs = hydraFunctions().filter((h) => !filterOut.includes(h.name))
     hydraFuncs.forEach((h) => {
         if (h.type === "combine" || h.type === "combineCoord") {
             h.inputs = [ { type: "vec4", name: "texture" }, ...h.inputs]
@@ -27,18 +39,25 @@ export default (hydra) => {
     })
 
     const functionOptions = hydraFuncs.map((h) => ({ 
-        label: `${h.name}()`, type: h.type, name: h.name,
+        label: `${h.name}()`, type: h.type, name: h.name, /*detail: h.type,*/
         info:  h.inputs !== undefined ? `${h.name}( ${h.inputs.map((input) => `${input.name}${input.default ? ` = ${input.default}`: ''}`).join(', ')} )` : ''
     }))
   
     functionOptions.push({ label: `out()` })
   
     const srcOptions = functionOptions.filter((h) => h.type === 'src')
+    const srcNames = srcOptions.map((h) => h.name)
     const chainOptions = functionOptions.filter((h) => h.type != 'src')
-    const outputOptions = hydraGlobals.filter((h) => h.type === 'Output')
-    const externalSourceOptions =  hydraGlobals.filter((h) => h.type === 'HydraSource')
+    const outputOptions = synthParams.filter((h) => h.type === 'Output')
+    const externalSourceOptions =  synthParams.filter((h) => h.type === 'HydraSource')
     // const textureOptions = [...outputOptions]
+
+    const userConstants = hydra.sandbox.userProps
+    //&& !userConstants.includes(h.label)
+    const usefulConstants = ['a', 'mouse', 'width', 'height', 'time']
+    const hydraConstants = synthParams.filter((h) => usefulConstants.includes(h.label))
     const combineNames = functionOptions.filter((h) => h.type === 'combine' || h.type === 'combineCoord').map((h) => h.name)
-    console.log('functionOptions', functionOptions)
-    return { srcOptions, chainOptions, hydraGlobals, outputOptions, externalSourceOptions, combineNames }
+    const hydraGlobals = synthParams.filter((h) => h.type === 'function' &&  !srcNames.includes(h.name)|| userConstants.includes(h.label))
+    console.log('functionOptions', functionOptions, srcNames, synthParams)
+    return { srcOptions, chainOptions, hydraGlobals, hydraConstants, outputOptions, externalSourceOptions, combineNames }
   }
